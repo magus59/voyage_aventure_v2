b4f6334a8207311881c0971cbe5caa9b6f24fe3e
Habib <hanbli.habib@gmail.com>
2024-07-01 13:54:27 +0200

    creation de la page d'acceuil

--- index.php
+++ index.php
@@ -1,14 +1,40 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/CSS/article.css">
-    <link rel="stylesheet" href="/CSS/commentaire.css">
-    <link rel="stylesheet" href="/CSS/utilisateur.css">
     <title>Document</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
+
+<div class="container">
+        <h1>Bienvenue</h1>
+        <div class="buttons">
+            <button  class="btn" onclick="redirectToCreateAccount()">
+                Créer un compte
+            </button>
+            <button  class="btn" onclick="redirectToConnect()">
+                Se connecter
+            </button>
+        </div>
+    </div>
+
+<script type="text/javascript">
+
+    // Function pour rediriger vers le fichier ClientController
+    function redirectToCreateAccount() {
+        // window.location est une function JS déjà crée qui nous permet avec le replace d'être rediriger vers un autre URL
+        window.location.replace('./Views/register.php');
+    }
+
+    function redirectToConnect() {
+        window.location.replace('./Controllers/CompteController.php');
+    }
+
     
+
+</script>
 </body>
 </html>
+
